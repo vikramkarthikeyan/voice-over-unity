d716c2fa9f9c57874950aab9a4e271cccdf0955e
Vikram Karthikeyan <vkarthik@buffalo.edu>
2019-02-16 19:27:57 -0500

    Speech to text Unity

---
+++ Speech to text/Assets/Scenes/PhoneCamera.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhoneCamera : MonoBehaviour
+{
+    private bool camAvailable;
+    private WebCamTexture backCam;
+    private Texture defaultBackground;
+
+    public RawImage background;
+    public AspectRatioFitter fit;
+
+    private void Start()
+    {
+        defaultBackground = background.texture;
+        WebCamDevice[] devices = WebCamTexture.devices;
+
+        if (devices.Length == 0)
+        {
+            Debug.Log("No Camera Detected");
+            camAvailable = false;
+            return;
+        }
+
+        for (int i = 0; i < devices.Length; i++)
+        {
+            if (devices[i].isFrontFacing)
+            {
+                backCam = new WebCamTexture(devices[i].name, Screen.width, Screen.height);
+            }
+        }
+
+        if (backCam == null)
+        {
+            Debug.Log("Unable to find back camera");
+            return;
+        }
+
+        backCam.Play();
+        background.texture = backCam;
+
+        camAvailable = true;
+    }
+
+    private void Update()
+    {
+        if (!camAvailable)
+        {
+            return;
+        }
+
+        float ratio = (float)backCam.width / (float)backCam.height;
+        fit.aspectRatio = ratio;
+
+        float scaleY = backCam.videoVerticallyMirrored ? -1f : 1f;
+        background.rectTransform.localScale = new Vector3(1f, scaleY);
+
+        int orient = -backCam.videoRotationAngle;
+        background.rectTransform.localEulerAngles = new Vector3(0, 0, orient);
+    }
+}
---
+++ Speech to text/Assets/SoundLevel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundLevel : MonoBehaviour
+{
+    // Start is called before the first frame update
+   private void Start()
+    {
+        var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        // color is controlled like this
+        Material m_material = GetComponent<Renderer>().material;
+        m_material.color = Color.magenta; // for example
+                                                    // There are lots more colours to choose
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+}
